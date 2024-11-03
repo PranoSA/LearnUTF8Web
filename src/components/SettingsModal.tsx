c127cfb87a0db60bb7ed9b50cf8d159d29e14450
PranoSA <pcadler@gmail.com>
2024-11-03 09:42:13 -0800

    better layout to change settings

--- src/components/SettingsModal.tsx
+++ src/components/SettingsModal.tsx
@@ -1,3 +1,6 @@
+//close icon
+import { FaTimes } from 'react-icons/fa';
+
 export type Settings = {
   utfVersion: 'utf-8' | 'utf-16' | 'utf-32';
   showHex: boolean;
@@ -27,7 +30,7 @@ const SettingsModal: React.FC<SettingsModalProps> = ({
 }) => {
   return (
     <div
-      className="fixed z-10 inset-0 overflow-y-auto"
+      className="fixed z-10 inset-0 overflow-y-auto w-full h-full flex items-center justify-center"
       onClick={closeSettingsModal}
     >
       <div className="fixed inset-0 bg-black opacity-50 items-center flex flex-row"></div>
@@ -36,9 +39,15 @@ const SettingsModal: React.FC<SettingsModalProps> = ({
         //make sure doesn't propagate to parent
       >
         <div
-          className="bg-white rounded-lg shadow-lg p-6  max-w-md mx-auto border-red-400 border-4"
+          className="relative bg-white rounded-lg shadow-lg p-6  max-w-md mx-auto border-blue-400 border-4"
           onClick={(e) => e.stopPropagation()}
         >
+          <FaTimes
+            className="absolute top-0 right-0 m-4 cursor-pointer"
+            onClick={closeSettingsModal}
+            size={30}
+          />
+
           <h2 className="text-xl font-bold mb-4">Settings</h2>
           <label className="flex items-center mb-2">
             <input
